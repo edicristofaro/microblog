4dcc4c18d37096afe47bc5fcefee0ebc57dba23f
Ed <edicristofaro@gmail.com>
2017-05-05 17:17:33 -0700

    Added Whoosh search on post body.

--- app/views.py
+++ app/views.py
@@ -2,10 +2,10 @@ from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 from app import app, db, lm
 from datetime import datetime
-from .forms import LoginForm, EditForm, PostForm
+from .forms import LoginForm, EditForm, PostForm, SearchForm
 from .models import User, Post
 from .oauth import OAuthSignIn
-from config import POSTS_PER_PAGE
+from config import POSTS_PER_PAGE, MAX_SEARCH_RESULTS
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -34,6 +34,7 @@ def before_request():
         g.user.last_seen = datetime.utcnow()
         db.session.add(g.user)
         db.session.commit()
+        g.search_form = SearchForm()
 
 @app.route('/login', methods=['GET', 'POST'])
 #@oid.loginhandler
@@ -149,6 +150,22 @@ def unfollow(nickname):
     db.session.commit()
     flash('You have stopped following ' + nickname + '.')
     return redirect(url_for('user', nickname=nickname))
+
+@app.route('/search', methods=['POST'])
+@login_required
+def search():
+    if not g.search_form.validate_on_submit():
+        return redirect(url_for('index'))
+    return redirect(url_for('search_results', query=g.search_form.search.data))
+    # Redirecting instead of returning search results from this route will stop a page refresh from confirming form re-submission
+
+@app.route('/search_results/<query>')
+@login_required
+def search_results(query):
+    results = Post.query.whoosh_search(query, MAX_SEARCH_RESULTS).all()
+    return render_template('search_results.html',
+                           query=query,
+                           results=results)
 
 @lm.user_loader
 def load_user(id):
